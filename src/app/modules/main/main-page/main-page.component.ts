6f024fa82fd7b3d105192bbef14bd553baceffa1
chananate <chananate.k@gmail.com>
2022-09-11 06:58:21 +0700

    mm

--- src/app/modules/main/main-page/main-page.component.ts
+++ src/app/modules/main/main-page/main-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { GetdataService } from 'src/app/service/getdata.service';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-main-page',
